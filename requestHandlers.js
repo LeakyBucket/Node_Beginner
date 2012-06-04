c3ee5353cc760b9fda50d3dda42aaf197239bc1c
Glen Holcomb <MentalLabor@gmail.com>
2012-06-04 13:12:08 -0600

    Showing how return doesn't work with blocking/slow requests.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,12 @@
 function start() {
   console.log("Request handler 'start' was called.");
+
+  function sleep(miliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + miliSeconds);
+  }
+
+  sleep(10000);
   return "Hello Start";
 }
 
