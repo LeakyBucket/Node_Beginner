bacf303c3408a973c17ddcfb921bf51a4e1ddb57
Glen Holcomb <MentalLabor@gmail.com>
2012-06-04 18:22:30 -0600

    /start handler now renders form with text area and submit button.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,31 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-  var content = "empty";
 
-  exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
-  });
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
 
-  return content;
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
